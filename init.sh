60896df8bd3c7f70f3406ab71ae7beb3d1094bf3
gq.guo <gq.guo@gqguo-Mac.local>
2020-08-07 14:16:22 +0800

    update init script

--- init.sh
+++ init.sh
@@ -1,19 +1,17 @@
 ###
  # @Author: gq.guo
  # @Date: 2020-08-04 17:07:36
- # @LastEditTime: 2020-08-06 17:57:28
+ # @LastEditTime: 2020-08-07 14:15:59
  # @LastEditors: gq.guo
  # @Description: In User Settings Edit
  # @FilePath: /api_test/init.sh
 ### 
 rm -rf $HOME/tmp/apitest/
 
-if [ ! -d "$HOME/tmp/api_test/apienv" ]; then
-  python3 -m venv apienv
-fi
-
-mv $HOME/.jenkins/workspace/api_test_data/* $HOME/tmp/api_test/
 cd $HOME/tmp/api_test/
+python3 -m venv apienv
+mv $HOME/.jenkins/workspace/api_test_data/* $HOME/tmp/api_test/
+
 
 source $HOME/tmp/api_test/apienv/bin/activate
 pip install httprunner
